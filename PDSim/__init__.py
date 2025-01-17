ac33495545afbacc2f1df29ebf732dea749756cd
Thomas W. Moesch <thomas.moesch2014@gmail.com>
2025-01-17 19:57:26 +0100

    edited screw/core.py to include ScrewSpindle with leakage and injection

--- PDSim/__init__.py
+++ PDSim/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '2.15'
-__git_revision__ = '140ddd739566ced52c289a7e3b6d99c4cf40f380'
+__version__ = '2.14'
+__git_revision__ = '1fb6de3158f08eefe84c3c9f72a79fd5abe4d882'
 __git_branch__ = 'screw_model'
